3284fbe16af64860cd28d40bbfd8b3c2619098cf
Patryczekk <patryk.sroka@elev.ga.ntig.se>
2020-10-23 13:09:21 +0200

    number game

---
+++ gissa talet/game.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Guess the random generate number between 1 - 10  ', (answer) => {
+
+
+    rl.close();
+});
+
+let Nmb = Math.floor(Math.random() * 25) + 1; 
+
+if (answer == Nmb) {
+    console.log("You win!!!")
+}
+else if (answer != Nmb){
+    console.log("You fucking dumb!! Try harder!!")
+}
