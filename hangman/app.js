0594cc6f34926aa89d76b861a29217f00675d19a
PatrykSroka21 <patryk.sroka01@gmail.com>
2020-11-26 14:33:48 +0100

    Update app.js

--- hangman/app.js
+++ hangman/app.js
@@ -1,1 +1,24 @@
-//
+//Get words to guess in a array for 3 different difficulties 
+
+//Welcome message || let the user choose diffiluty
+
+//If statement for chosing a different difficulty
+
+//Get a random word from chosen difficulty array 
+
+//Split the word and tell the length of a word
+
+//Let the user choose if he is guessing the whole word or a single letter
+ 
+//For loop for tries
+
+//If statement for right/wrong guess on the letter
+ 
+//If statement for right/wrong guess on the word
+
+//If statement for if tries = 0 to end the game
+
+//Ask the user to play again
+
+//If statement for playing again or closing the application 
+
